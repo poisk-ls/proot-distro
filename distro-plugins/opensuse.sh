4e646b0e9c03e26f0e085a0c4453dfdd322db45a
Jade <112060601+poisk-ls@users.noreply.github.com>
2022-11-08 22:03:59 +0800

    Add files via upload

---
+++ distro-plugins/opensuse.sh
@@ -0,0 +1,13 @@
+# This is a default distribution plug-in.
+# Do not modify this file as your changes will be overwritten on next update.
+# If you want customize installation, please make a copy.
+DISTRO_NAME="OpenSUSE (Tumbleweed)"
+
+TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v2.8.0/opensuse-aarch64-pd-v2.8.0.tar.xz"
+TARBALL_SHA256['aarch64']="b28a0fa1a30f2f466515c16e6c448bc3dcba13419477952a9637f646d0a2df39"
+TARBALL_URL['arm']="https://github.com/termux/proot-distro/releases/download/v2.8.0/opensuse-arm-pd-v2.8.0.tar.xz"
+TARBALL_SHA256['arm']="67bca0544264eefb145830cd8273496f4d76407fe87e27b89e565164594e5b38"
+TARBALL_URL['i686']="https://github.com/termux/proot-distro/releases/download/v2.8.0/opensuse-i686-pd-v2.8.0.tar.xz"
+TARBALL_SHA256['i686']="c99559ba5671ab60295c925bdda3640c07e8700bbb560c45b29eed14debca14e"
+TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v2.8.0/opensuse-x86_64-pd-v2.8.0.tar.xz"
+TARBALL_SHA256['x86_64']="84ad2671c05f9d68825fe2f11fd7657f045e3625c7b8f111e5313bc34914882b"
